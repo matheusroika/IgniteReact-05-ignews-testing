8c1d72d792a14232184006eb240b88287b0eb7f2
Matheus Roika <matheusroika@hotmail.com>
2021-06-14 17:26:30 -0300

    Testando getStaticProps Nesta aula aprendemos sobre o mockResolvedValue, que serve para mockar funções que retornam promises.

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../../pages";
+import { stripe } from "../../services/stripe";
+import Home, { getStaticProps } from "../../pages";
+import { mocked } from "ts-jest/utils";
 
 jest.mock("next/router");
 jest.mock("next-auth/client", () => {
@@ -7,11 +9,33 @@ jest.mock("next-auth/client", () => {
     useSession: () => [null, false],
   };
 });
+jest.mock("../../services/stripe");
 
 describe("Home page", () => {
   it("renders correctly", () => {
-    render(<Home product={{ priceId: "fake-price-id", amount: "R$10,00" }} />);
+    render(<Home product={{ priceId: "fake-price-id", amount: "$10.00" }} />);
 
-    expect(screen.getByText(/R\$10,00/)).toBeInTheDocument();
+    expect(screen.getByText(/\$10\.00/)).toBeInTheDocument();
+  });
+
+  it("loads initial data via getStaticProps", async () => {
+    const retrieveStripePricesMocked = mocked(stripe.prices.retrieve);
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      id: "fake-price-id",
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "fake-price-id",
+            amount: "$10.00",
+          },
+        },
+      })
+    );
   });
 });
